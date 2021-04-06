cf54d3d6798756b6922088eba736b674d652cbba
Carlo Moretti <carlo@duckma.com>
2021-04-06 22:46:43 +0200

    again

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,3 @@
-var request = new XMLHttpRequest();
-request.open('GET', "https://onheiron.github.io/birds-vs-bats/demo.gb", true);
-request.responseType = 'blob';
-request.onload = function() {
-    var reader = new FileReader();
-    reader.readAsDataURL(request.response);
-    reader.onload =  function(e){
-        console.log('DataURL:', e.target.result);
-        var canvas = document.getElementById('gameCanvas');
-        var input = document.getElementById('file');
-        input.value = e.target.result;
-        new GameboyJS.Gameboy(canvas, { zoom: 2 });
-    };
-};
-request.send();
+var canvas = document.getElementById("gameCanvas");
+console.log(GameboyJS.RomAjaxReader);
+new GameboyJS.Gameboy(canvas, { zoom: 2, romReaders: [ (new GameboyJS.RomAjaxReader()).loadFromUrl("https://onheiron.github.io/birds-vs-bats/demo.gb") ] });
