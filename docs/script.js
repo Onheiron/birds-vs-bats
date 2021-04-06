89a1f7020be41e90564188cc99b7c723e269934f
Carlo Moretti <carlo@duckma.com>
2021-04-06 22:48:26 +0200

    almost

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,2 @@
 var canvas = document.getElementById("gameCanvas");
-console.log(GameboyJS.RomAjaxReader);
 new GameboyJS.Gameboy(canvas, { zoom: 2, romReaders: [ (new GameboyJS.RomAjaxReader()).loadFromUrl("https://onheiron.github.io/birds-vs-bats/demo.gb") ] });
